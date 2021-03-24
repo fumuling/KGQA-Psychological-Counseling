cf7d76c1cfc93920820988fc011e628808216d7f
fumuling <51187109+fumuling@users.noreply.github.com>
2021-03-24 19:32:20 +0800

    爬虫与json数据完成

--- data/preProcessing.py
+++ data/preProcessing.py
@@ -1,1 +1,10 @@
-import request
+import json
+import efaqa_corpus_zh
+
+def get_corpus():
+    l = list(efaqa_corpus_zh.load())
+    print("size: %s" % len(l))
+    print(l[2]['title'])
+
+
+get_corpus()
